b95bf64f874a91352ec0e7048f13654747ae4da5
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2024-04-09 00:00:18 -0300

    Removendo BookStatus

--- src/main/java/com/example/bookmanagement/bookStatus/BookStatusRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.bookmanagement.bookStatus;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookStatusRepository extends JpaRepository<BookStatus, Long> {
-}
